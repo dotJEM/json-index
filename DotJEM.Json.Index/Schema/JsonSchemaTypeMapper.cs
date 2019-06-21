0e9943f2147e9c7fdc4019d3080597a93a15b57c
Elena Smeu <elena.smeu@systematic.com>
2019-06-21 12:29:16 +0200

    Schema Type and Extended Type--fix

--- DotJEM.Json.Index/Schema/JsonSchemaTypeMapper.cs
+++ DotJEM.Json.Index/Schema/JsonSchemaTypeMapper.cs
@@ -21,20 +21,20 @@ namespace DotJEM.Json.Index.Schema
             map[JTokenType.Null] = JsonSchemaType.Null;
 
             //NOTE: Unsupported types, perhaps these should be any instead?
-            map[JTokenType.Date] = JsonSchemaType.Any;
+            map[JTokenType.Date] = JsonSchemaType.String;
             map[JTokenType.Raw] = JsonSchemaType.Any;
             map[JTokenType.Bytes] = JsonSchemaType.Any;
-            map[JTokenType.Guid] = JsonSchemaType.Any;
-            map[JTokenType.Uri] = JsonSchemaType.Any;
-            map[JTokenType.TimeSpan] = JsonSchemaType.Any;
-            map[JTokenType.Undefined] = JsonSchemaType.Any;
+            map[JTokenType.Guid] = JsonSchemaType.String;
+            map[JTokenType.Uri] = JsonSchemaType.String;
+            map[JTokenType.TimeSpan] = JsonSchemaType.String;
+            map[JTokenType.Undefined] = JsonSchemaType.Null;
 
-            //NOTE: We should not be meeting these:
-            //typeMapping[JTokenType.Constructor] = JsonSchemaType.None;
-            //typeMapping[JTokenType.Property] = JsonSchemaType.None;
-            //typeMapping[JTokenType.Comment] = JsonSchemaType.None;
-            //typeMapping[JTokenType.Undefined] = JsonSchemaType.None;
-            //typeMapping[JTokenType.Constructor] = JsonSchemaType.None;
+        //NOTE: We should not be meeting these:
+        //    typeMapping[JTokenType.Constructor] = JsonSchemaType.None;
+        //    typeMapping[JTokenType.Property] = JsonSchemaType.None;
+        //    typeMapping[JTokenType.Comment] = JsonSchemaType.None;
+        //    typeMapping[JTokenType.Undefined] = JsonSchemaType.None;
+        //    typeMapping[JTokenType.Constructor] = JsonSchemaType.None;
         }
 
         public static JsonSchemaType ToSchemaType(this JTokenType self)
