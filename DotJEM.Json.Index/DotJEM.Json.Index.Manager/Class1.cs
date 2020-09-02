2973391476bb4d54d34391f59cf8148704f29b27
Jens Melgaard <admin@it-links.dk>
2020-09-02 22:04:32 +0200

    Add manager project,.

---
+++ DotJEM.Json.Index/DotJEM.Json.Index.Manager/Class1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using DotJEM.Json.Index.IO;
+using Newtonsoft.Json.Linq;
+
+
+namespace DotJEM.Json.Index.Manager
+{
+    public interface IIndexSyncronizationHandler
+    {
+    }
+
+    public class IndexSyncronizationHandler : IIndexSyncronizationHandler
+    {
+        private ILuceneJsonIndexDataSource source;
+        private ILuceneJsonIndex index;
+
+
+        public void Initialize()
+        {
+            source.Subscribe(new FuncObserver(OnSource));
+        }
+
+        public void OnSource(JObject entity)
+        {
+            IJsonIndexWriter writer = index.CreateWriter();
+            //writer.Update()
+
+        }
+
+        public ISnapshotInfo TakeSnapshot()
+        {
+            return null;
+        }
+    }
+
+    public class FuncObserver : IObserver<JObject>
+    {
+        private readonly Action<JObject> onSource;
+
+        public FuncObserver(Action<JObject> onSource)
+        {
+            this.onSource = onSource;
+        }
+
+        public void OnCompleted() { }
+
+        public void OnError(Exception error) { }
+
+        public void OnNext(JObject value) => onSource(value);
+    }
+
+    /// <summary>
+    /// In context of DotJEM.Json.Storage as a DataSource, a data source can be one or more storage areas, this 
+    /// </summary>
+    public interface ILuceneJsonIndexDataSource : IObservable<JObject>
+    {
+
+
+        ISnapshotInfo TakeSnapshot();
+    }
+
+    class LuceneJsonIndexDataSource : ILuceneJsonIndexDataSource
+    {
+        public IDisposable Subscribe(IObserver<JObject> observer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ISnapshotInfo TakeSnapshot()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface ISnapshotInfo
+    {
+
+    }
+
+
+}
