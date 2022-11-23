86bc16a9e78d1dac64a09142757c0af8b6f101e0
Jens Melgaard <admin@it-links.dk>
2022-11-23 15:13:59 +0100

    implementing snapshots

--- DotJEM.Json.Index/IIndexStorage.cs
+++ DotJEM.Json.Index/IIndexStorage.cs
@@ -1,5 +1,8 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
+using System.Threading;
 using DotJEM.Json.Index.Storage;
 using Lucene.Net.Analysis;
 using Lucene.Net.Analysis.Standard;
@@ -17,8 +20,9 @@ namespace DotJEM.Json.Index
         bool Exists { get; }
         void Close();
         void Flush();
-
         bool Purge();
+        bool Snapshot(ISnapshot snapshot);
+        bool Restore(ISnapshot snapshot);
     }
 
     public abstract class AbstractLuceneIndexStorage : IIndexStorage
@@ -31,6 +35,7 @@ namespace DotJEM.Json.Index
         private IndexWriter writer;
         private IndexReader reader;
         private Analyzer analyzer;
+        private SnapshotDeletionPolicy deletePolicy = new SnapshotDeletionPolicy(new KeepOnlyLastCommitDeletionPolicy());
 
         protected AbstractLuceneIndexStorage(Directory directory)
         {
@@ -42,8 +47,7 @@ namespace DotJEM.Json.Index
             //TODO: The storage should define the analyzer, not the writer.
             lock (padlock)
             {
-                var policy = new SnapshotDeletionPolicy(new KeepOnlyLastCommitDeletionPolicy());
-                return writer ?? (writer = new IndexWriter(Directory, this.analyzer = analyzer, !Exists, policy, IndexWriter.MaxFieldLength.UNLIMITED));
+                return writer ??= new IndexWriter(Directory, this.analyzer = analyzer, !Exists, deletePolicy, IndexWriter.MaxFieldLength.UNLIMITED);
             }
         }
 
@@ -77,18 +81,50 @@ namespace DotJEM.Json.Index
                 Close();
                 if (analyzer == null)
                 {
-                    var temp = new IndexWriter(Directory, new SimpleAnalyzer(), true, IndexWriter.MaxFieldLength.UNLIMITED);
+                    var temp = new IndexWriter(Directory, new SimpleAnalyzer(), true, deletePolicy, IndexWriter.MaxFieldLength.UNLIMITED);
                     temp.Commit();
                     temp.Dispose();
                 }
                 else
                 {
-                    writer = new IndexWriter(Directory, analyzer, true, IndexWriter.MaxFieldLength.UNLIMITED);
+                    writer = new IndexWriter(Directory, analyzer, true, deletePolicy, IndexWriter.MaxFieldLength.UNLIMITED);
                     writer.Commit();
                 }
             }
 
             return true;
+        }
+
+        public bool Snapshot(ISnapshot snapshot)
+        {
+            if (writer == null)
+                return false;
+
+            if (snapshot == null) throw new ArgumentNullException(nameof(snapshot));
+
+            lock (padlock)
+            {
+                IndexCommit commit = deletePolicy.Snapshot();
+                try
+                {
+                    foreach (string file in commit.FileNames)
+                    {
+                        using IndexInputStream source = new (file, Directory.OpenInput(file));
+                        snapshot.WriteFile(source);
+                    }
+                }
+                finally
+                {
+                    deletePolicy.Release();
+                }
+            }
+
+            return true;
+        }
+
+        public bool Restore(ISnapshot snapshot)
+        {
+            throw new NotImplementedException();
         }
 
         public void Flush()
@@ -98,6 +134,11 @@ namespace DotJEM.Json.Index
                 writer?.Flush(true, true, true);
             }
         }
+    }
+
+    public interface ISnapshot
+    {
+        void WriteFile(IndexInputStream input);
     }
 
     public class LuceneMemmoryIndexStorage : AbstractLuceneIndexStorage
@@ -132,6 +173,78 @@ namespace DotJEM.Json.Index
             //Note: Ensure cacheDirectory.
             : base(new MMapDirectory(System.IO.Directory.CreateDirectory(path)))
         {
+        }
+    }
+
+    public class IndexInputStream : Stream
+    {
+        public string FileName { get; }
+        public IndexInput IndexInput { get; }
+
+        public IndexInputStream(string fileName, IndexInput indexInput)
+        {
+            FileName = fileName;
+            IndexInput = indexInput;
+        }
+
+        public override void Flush()
+        {
+            throw new InvalidOperationException("Cannot flush a readonly stream.");
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            switch (origin)
+            {
+                case SeekOrigin.Begin:
+                    Position = offset;
+                    break;
+                case SeekOrigin.Current:
+                    Position += offset;
+                    break;
+                case SeekOrigin.End:
+                    Position = Length - offset;
+                    break;
+            }
+            return Position;
+        }
+
+        public override void SetLength(long value)
+        {
+            throw new InvalidOperationException("Cannot change length of a readonly stream.");
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            int remaining = (int)(IndexInput.Length() - IndexInput.FilePointer);
+            int readCount = Math.Min(remaining, count);
+            IndexInput.ReadBytes(buffer, offset, readCount);
+            return readCount;
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            throw new InvalidCastException("Cannot write to a readonly stream.");
+        }
+
+        public override bool CanRead => true;
+        public override bool CanSeek => true;
+        public override bool CanWrite => false;
+        public override long Length => IndexInput.Length();
+
+        public override long Position
+        {
+            get => IndexInput.FilePointer;
+            set => IndexInput.Seek(value);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                IndexInput.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
